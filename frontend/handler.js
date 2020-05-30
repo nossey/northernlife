ec0a6b5b3b89dc1566036a78256dbf662738f965
Sho Kitami <shokitami@gmail.com>
2020-05-30 20:29:01 +0900

    backup

--- frontend/handler.js
+++ frontend/handler.js
@@ -1,13 +1,26 @@
 const https = require('https');
 let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html";
 
-exports.render = async function(event) {
-  const promise = new Promise(function(resolve, reject) {
-    https.get(url, (res) => {
-      resolve(res.statusCode)
-    }).on('error', (e) => {
-      reject(Error(e))
-    })
-  });
-  return promise;
+exports.render = async function(event, context) {
+      const response = {
+
+        foo: 'Hello, world',
+        bar: 'Goodbye, world',
+        event: event, // 呼び出し元からの情報を見たいので出力に含める
+        context: context, // コンテキスト情報を見たいので出力に含める
+
+        // Amazon API Gateway を利用して Lambda 関数を WebAPI 化するときは
+        // 必要に応じて isBase64Encoded, statusCode, headers, body をセットする。
+        // 今回は不要だがサンプル的にセットしておく。
+        statusCode: 200, // HTTP 200 OK
+        headers: {
+            'x-custom-header': 'my custom header value'
+        },
+        body: JSON.stringify({
+            foo: 'Hello, world',
+            bar: 'Goodbye, world',
+        })
+    };
+
+    return response;
 }
