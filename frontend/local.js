c4b920b96bb68eb5e1a7c05ba599c809882fdce1
Sho Kitami <shokitami@gmail.com>
2020-06-04 23:25:12 +0900

    local files added

---
+++ frontend/local.js
@@ -0,0 +1,11 @@
+const { Builder } = require('nuxt');
+const { app, nuxt } = require('./app');
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+(async function main() {
+  if (!IS_PROD){
+     await new Builder(nuxt).build();
+  }
+
+  app.listen(3000);
+})();
