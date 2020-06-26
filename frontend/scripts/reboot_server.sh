b048ad11a2d8c8d8d3358d8b4bdadbfd701f8cd3
Sho Kitami <shokitami@gmail.com>
2020-06-27 02:56:51 +0900

    fix scripts

--- frontend/scripts/reboot_server.sh
+++ frontend/scripts/reboot_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -a
-source .env
-set +a
+sudo set -a
+sudo source .env
+sudo set +a
 
-docker pull nossey/northernlife-frontend:$DOCKER_TAG
-docker run -p 80:8000 -d nossey/northernlife-frontend:$DOCKER_TAG > /dev/null 2>&1 &
+sudo docker pull nossey/northernlife-frontend:$DOCKER_TAG
+sudo docker run -p 80:8000 -d nossey/northernlife-frontend:$DOCKER_TAG > /dev/null 2>&1 &
