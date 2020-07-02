6a6bfdae2d2eb21231ebaa52d2753c3ae7ccfd9c
Sho Kitami <shokitami@gmail.com>
2020-07-02 22:04:17 +0900

    fix script

--- frontend/scripts/reboot_server.sh
+++ frontend/scripts/reboot_server.sh
@@ -4,8 +4,10 @@ sudo set -a
 sudo source .env
 sudo set +a
 
-echo $DOCKER_TAG
+#echo $DOCKER_TAG
 
-docker pull nossey/northernlife-frontend:$DOCKER_TAG
+docker pull nossey/northernlife-frontend:latest
+#docker pull nossey/northernlife-frontend:$DOCKER_TAG
+
 #docker run -p 8000:3000 -d nossey/northernlife-frontend:$DOCKER_TAG > /dev/null 2>&1 &
-docker run -p 8000:3000 -d nossey/northernlife-frontend:$DOCKER_TAG
+docker run -p 8000:3000 -d nossey/northernlife-frontend:latest
