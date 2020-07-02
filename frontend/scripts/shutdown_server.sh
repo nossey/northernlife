1786da732827f62815410d867da930b5fbc8b54c
Sho Kitami <shokitami@gmail.com>
2020-07-03 01:07:56 +0900

    fix script

--- frontend/scripts/shutdown_server.sh
+++ frontend/scripts/shutdown_server.sh
@@ -2,8 +2,11 @@
 
 sudo service nginx stop
 
-PID=`sudo lsof -t -i :8000`
+#PID=`sudo lsof -t -i :8000`
+#
+#if [ -n "$PID" ]; then
+#  sudo kill $PID
+#fi
 
-if [ -n "$PID" ]; then
-  sudo kill $PID
-fi
+CONTAINER_ID=`sudo -S docker ps -q  --filter ancestor=nossey/northernlife-frontend:latest`
+docker kill $CONTAINER_ID
