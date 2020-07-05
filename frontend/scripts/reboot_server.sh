6b5ee0d855083dae0ac2bff86c62685741d46271
Sho Kitami <shokitami@gmail.com>
2020-07-05 13:49:13 +0900

    remove unnecessary comments

--- frontend/scripts/reboot_server.sh
+++ frontend/scripts/reboot_server.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#sudo set -a
-#sudo source .env
-#sudo set +a
-
-#echo $DOCKER_TAG
-
 docker pull nossey/northernlife-frontend:latest
-#docker pull nossey/northernlife-frontend:$DOCKER_TAG
-
-#docker run -p 8000:3000 -d nossey/northernlife-frontend:$DOCKER_TAG > /dev/null 2>&1 &
 docker run -p 8000:3000 -d nossey/northernlife-frontend:latest
-
 sudo service nginx start
