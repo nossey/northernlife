e5d007fd10d0458af89cda19426ef2900b471b42
Sho Kitami <shokitami@gmail.com>
2020-07-05 12:50:32 +0900

    remove unnecessary comments

--- frontend/scripts/shutdown_server.sh
+++ frontend/scripts/shutdown_server.sh
@@ -2,12 +2,5 @@
 
 sudo service nginx stop
 
-#PID=`sudo lsof -t -i :8000`
-#
-#if [ -n "$PID" ]; then
-#  sudo kill $PID
-#fi
-
 CONTAINER_ID=`sudo -S docker ps -q  --filter ancestor=nossey/northernlife-frontend:latest`
 docker kill $CONTAINER_ID
-echo $CONTAINER_ID
