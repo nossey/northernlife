97f55348fa75ea6cebb8eafe54dd809ba8a4f429
Sho Kitami <shokitami@gmail.com>
2020-10-25 17:17:00 +0900

    feature:Added scss loader and header

---
+++ frontend/src/components/molecules/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header"><nuxt-link to="/">Northernlife</nuxt-link></div>
+</template>
+
+<script>
+export default {
+name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/colors.scss";
+
+.header {
+  background-color: $main-theme;
+  height: 40px;
+
+  a {
+    color: $font-color-white;
+    text-decoration: none;
+  }
+}
+
+</style>
