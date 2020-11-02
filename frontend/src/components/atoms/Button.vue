6446e311ddf7093410e6b15404a8a0ce4c49cc83
Sho Kitami <shokitami@gmail.com>
2020-11-02 22:01:07 +0900

    Added base button

---
+++ frontend/src/components/atoms/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button type="button"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: "Button",
+});
+
+</script>
+
+<style lang="scss" scoped>
+@import "assets/colors";
+
+button {
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+  padding: 5px 10px;
+  appearance: none;
+  border-radius: 5%;
+  color: $main-theme;
+  border: solid $main-theme 1px;
+  transition: background-color .4s;
+  font-weight: 480;
+
+  &:hover {
+    transition: background-color .4s;
+    background-color: $main-theme;
+    color: $font-color-white;
+  }
+}
+
+</style>
