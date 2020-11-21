d8879a57b1c9205eb6755c9b7022faea059217d5
Sho Kitami <shokitami@gmail.com>
2020-11-21 20:51:35 +0900

    fix

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@ import globalAxios from 'axios';
 export default ({store}) => {
   globalAxios.interceptors.request.use(req => {
     const token = store.$auth.getToken('local');
-    if (token && !globalAxios.defaults.headers.Authorization)
+    if (token && !req.headers.Authorization)
     {
       req.headers.Authorization = token;
     }
