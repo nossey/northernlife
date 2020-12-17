8cb905357d2e9e7a643398a79851f0edf67d3884
Sho Kitami <shokitami@gmail.com>
2020-12-18 01:33:54 +0900

    feature added ogp

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -14,7 +14,16 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+
+      // OGP
+      { hid: 'description', name: 'description', content: 'Northernlife' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Northernlife' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://northernlife.net' },
+      { hid: 'og:title', property: 'og:title', content: 'Northernlife' },
+      { hid: 'og:description', property: 'og:description', content: 'Northernlife' },
+      { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/northernlife.ico' }
