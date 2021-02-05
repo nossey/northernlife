e6921198b755dc2acd3841bbbe65306839c29b98
Sho Kitami <shokitami@gmail.com>
2021-02-05 21:52:05 +0900

    feature:aadded search form

---
+++ frontend/src/components/atoms/SearchForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="submit">
+    <input type="search" placeholder="検索">
+  </form>
+</template>
+
+<script lang="ts">
+
+import {defineComponent} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+ setup(){
+   const submit = () => {
+     console.log("Hello")
+   }
+
+   return {
+     submit
+   }
+ }
+
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+form {
+  input {
+    border: none;
+    padding: 3px;
+  }
+}
+
+</style>
