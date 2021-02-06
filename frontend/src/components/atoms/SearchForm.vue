c43580c13e6b7c2ab58924dcbbb10c10a906d860
Sho Kitami <shokitami@gmail.com>
2021-02-07 01:20:46 +0900

    feature:added fontawesome

--- frontend/src/components/atoms/SearchForm.vue
+++ frontend/src/components/atoms/SearchForm.vue
@@ -1,12 +1,15 @@
 <template>
   <form @submit.prevent="submit">
+    <fa :icon="faSearch"></fa>
     <input type="search" placeholder="検索">
   </form>
 </template>
 
 <script lang="ts">
 
-import {defineComponent} from "@nuxtjs/composition-api";
+import {defineComponent, computed} from "@nuxtjs/composition-api";
+
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export default defineComponent({
  setup(){
@@ -15,7 +18,8 @@ export default defineComponent({
    }
 
    return {
-     submit
+     submit,
+     faSearch
    }
  }
 
@@ -24,11 +28,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import "assets/colors";
 
 form {
+  display: inline-block;
+  border-radius: 4px;
+  background: $background-white;
+  svg {
+    color: $shadow-color;
+    margin-left: 10px;
+  }
   input {
     border: none;
     padding: 3px;
+    outline: none;
   }
 }
 
