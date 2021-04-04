eca9a2528ca9f85a8e62041ce84d2b0b79bd7929
Sho Kitami <shokitami@gmail.com>
2021-04-04 22:30:21 +0900

    update _id

--- frontend/src/pages/posts/_id.vue
+++ frontend/src/pages/posts/_id.vue
@@ -2,12 +2,14 @@
   <b-container class="mt-4">
     <b-row>
       <b-col cols="12" md="9">
-        <Post
-          :title="title"
-          :body="body"
-          :thumbnail="thumbnail"
-          :tags="tags"
-          :linkList="tagLinks"
+        <div v-if="fetchState.pending">Loading...</div>
+        <div v-else-if="fetchState.error">{{fetchState.error.message}}</div>
+        <Post v-else
+          :title="state.title"
+          :body="state.body"
+          :thumbnail="state.thumbnail"
+          :tags="state.tags"
+          :linkList="state.tagLinks"
         ></Post>
       </b-col>
       <b-col md="3" class="d-none d-md-block">Table of contents</b-col>
@@ -18,51 +20,46 @@
 <script>
 import { PostsApi } from "~/client";
 import { buildConfiguration } from "~/client/configurationFactory"
-import Post  from "~/components/molecules/Post.vue"
+import { defineComponent, reactive, computed, useFetch, useContext, useMeta } from "@nuxtjs/composition-api"
+import Post from "~/components/molecules/Post.vue"
 import Enumerable from "linq"
 
-export default {
+export default defineComponent({
   components: {
    Post
   },
-  head(){
-    return {
-      title: this.title,
-      meta: [
-        // OGP
-        { hid: 'og:title', property: 'og:title', content: this.title },
-        { hid: 'og:description', property: 'og:description', content: this.plainBody },
-        { hid: 'og:image', property: 'og:image', content: this.thumbnail },
-      ]
+  head: {},
+  setup(){
+    const state = reactive({
+      thumbnail: "",
+      title: "",
+      body: "",
+      plainBody: "",
+      tags: [],
+      tagLinks: computed(()=>{
+        const links = Enumerable.from(state.tags).select(function(t){return {name: t, link: `/?tag=${encodeURIComponent(t)}`}}).toArray()
+        return {links: links}
+      })
+    });
+
+    const { meta } = useMeta();
+    const {_, fetchState} = useFetch(async() => {
+      const api = new PostsApi(buildConfiguration());
+      const id = useContext().params.value["id"];
+      const post = (await api.postsIdGet(id)).data;
+      state.thumbnail = post.thumbnail;
+      state.title = post.title;
+      state.body = post.body;
+      state.plainBody = post.plainBody;
+      state.tags = (post.tags) ? post.tags : [];
+    });
+
+  return {
+      state,
+      fetchState
     }
-  },
-  async asyncData(ctx){
-    const api = new PostsApi(buildConfiguration());
-    const id = ctx.route.params['id'];
-    try
-    {
-      const post = (await api.postsIdGet(id)).data
-      const links = Enumerable.from(post.tags).select(function(t) { return {name: t, link: `/?tag=${encodeURIComponent(t)}`}}).toArray()
-      return {
-        thumbnail: post.thumbnail,
-        title: post.title,
-        body: post.body,
-        tags: post.tags,
-        tagLinks: {links: links}
-      }
-    }
-    catch (error)
-    {
-      ctx.error({statusCode: error.response.status});
-      return {
-        thumbnail: "",
-        title: "",
-        body: "",
-        tags: []
-      }
-    }
-  },
-}
+  }
+})
 </script>
 
 <style scoped>
